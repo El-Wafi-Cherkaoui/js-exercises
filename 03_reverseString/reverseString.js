687195cfd46a1f99f79f67472af0580d298c7211
el wafi cherkaoui <elwaficherkaoui@gmail.com>
2024-03-13 02:26:07 +0000

    solved the first four exercices

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let list = []
+    for (let i = 0; i<text.length; i++){
+        list[i] = text[i]
+    }
+    list = list.reverse()
+    let ne_txt = ""
+    for (let i = list.length-1; i>= 0; i--){
+        ne_txt += text[i]
+    }
+    return ne_txt
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list, element) {
+    let newt
+    if (arguments.length >2){
+        for (let i = 1; i < arguments.length; i++ ){
+        newt = list.filter((x)=>{ return x !==  arguments[i]})
+        list = newt
+        }
+        return newt
+    }
+    else{
+        newt = list.filter((x)=>{ return x !==  element})        
+        return newt
+    }
+    
 };
 
 // Do not edit below this line
