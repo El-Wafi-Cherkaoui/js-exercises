4b22a75abc4dbd7ebe87eb7e025d72d9264c2659
el wafi cherkaoui <elwaficherkaoui@gmail.com>
2024-03-14 14:53:10 +0000

    SOLVED

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((theOldestPerson, current)=>{
+        if (theOldestPerson.yearOfDeath == undefined) theOldestPerson.yearOfDeath = new Date().getFullYear()
+        if (current.yearOfDeath == undefined) current.yearOfDeath = new Date().getFullYear()
+        
+        let current_age = current.yearOfDeath - current.yearOfBirth 
+        let theOldestPerson_age = theOldestPerson.yearOfDeath - theOldestPerson.yearOfBirth
+        
+        if (current_age > theOldestPerson_age) return current
+        return theOldestPerson
+        }, array[0])
 };
 
 // Do not edit below this line
