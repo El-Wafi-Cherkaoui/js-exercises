40ca542d8678e06027cecf236dfb143dde8819ef
el wafi cherkaoui <elwaficherkaoui@gmail.com>
2024-03-17 01:35:59 +0000

    remove non functional lines of code

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,9 +1,7 @@
 const fibonacci = function(num) {
     if (num <0) return "OOPS"
     let fib_nums = [0,1,1]
-    let count = 2
     for (let i=1; i<num; i++){
-        count += 1
         fib_nums.push(fib_nums[i]+ fib_nums[i+1])
     }
     return fib_nums[num]
