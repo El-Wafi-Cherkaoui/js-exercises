c31ebc4674e25a48d1f63ef8c9935a680437268d
el wafi cherkaoui <elwaficherkaoui@gmail.com>
2024-03-17 01:30:55 +0000

    Solve it more efficiently

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,21 +1,10 @@
 const palindromes = function (text) {
-    let alphabits = []
-    let row_text = ""
-    // extract row text without spaces or ! , .  into array
-    for (let i=0; i <text.length; i++){
-        if (text[i]== " " || text[i]== "!" || text[i]== "."|| text[i]== ",") continue
-        alphabits.push(text[i])
-        row_text += text[i]
-    }
-    // reverse the order
-    alphabits = alphabits.reverse()
-    let output = ""
-    // turn array into string
-    for (let i=0 ; i <alphabits.length; i++){
-        output += alphabits[i]
-    }
-    // compare the two 
-    return output.toUpperCase() == row_text.toUpperCase()
+    let symboles = [" ", "?", "!", ".", ","]
+    let local_copy = text.toLowerCase().split('').filter((char)=>!symboles.includes(char)).join("")
+    let rev_copy = local_copy.split('').reverse().join('') 
+    console.log(local_copy, rev_copy)
+    if (local_copy == rev_copy) return true
+    else return false
 };
 
 // Do not edit below this line
