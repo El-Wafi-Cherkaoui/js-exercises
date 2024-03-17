4b8455de15612099ba901313ca0ca254c5f9eb7f
el wafi cherkaoui <elwaficherkaoui@gmail.com>
2024-03-17 01:41:18 +0000

    use map method on array instead of traditional For loops

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,9 +1,5 @@
 const getTheTitles = function(array) {
-    let titles = []
-    array.forEach(element => {
-        titles.push(element.title)
-    });
-    return titles
+    return array.map((book)=>book.title)
 };
 
 // Do not edit below this line
